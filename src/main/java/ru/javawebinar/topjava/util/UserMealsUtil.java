aa3cc96257e5fa18589977843e79dbe339bd7389
Yehhor Kaletinsky <yehhor.k@mail.ru>
2016-05-27 11:52:58 +0300

    lambdas applied

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,8 +6,8 @@ import ru.javawebinar.topjava.model.UserMealWithExceed;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * GKislin
@@ -16,20 +16,38 @@ import java.util.List;
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 27,10,0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 27,13,0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 27,20,0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 27,20,0), "Завтрак", 1000), // 3000 cal
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 22,10,0), "Завтрак", 1000), // 1000 cal
+                new UserMeal(LocalDateTime.of(2015, Month.JANUARY, 31,13,0), "Обед", 5000), // 500 cal
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510) // 2510 cal
         );
-        getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+        getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(22,0), 2000).stream().
+        forEach(System.out::println);
+
     }
 
     public static List<UserMealWithExceed>  getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+
+        if (mealList == null || startTime == null || endTime == null)
+            return null;
+
+        List<UserMealWithExceed> list = mealList.parallelStream().map(userMeal -> new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(),
+                userMeal.getCalories(),
+                mealList.parallelStream().
+                        filter(userMeal1 ->
+                userMeal.getDateTime().toLocalDate().equals(userMeal1.getDateTime().toLocalDate()) &&
+                        TimeUtil.isBetween(userMeal1.getDateTime().toLocalTime(), startTime, endTime)).
+                        mapToInt(UserMeal::getCalories).sum() > caloriesPerDay)).collect(Collectors.toList()).
+                stream().sorted().collect(Collectors.toList());
+
+
+        return list;
     }
 }
