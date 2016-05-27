97f143c3b2ac5f5b062194eebd517485be9b23ad
Yehhor Kaletinsky <yehhor.k@mail.ru>
2016-05-27 12:47:27 +0300

    patch applied

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -6,13 +6,7 @@ import java.time.LocalDateTime;
  * GKislin
  * 11.01.2015.
  */
-public class UserMealWithExceed implements Comparable<UserMealWithExceed>{
-
-    private static int globalCounter = 0;
-
-    private int counter = 0;
-
-
+public class UserMealWithExceed {
     protected final LocalDateTime dateTime;
 
     protected final String description;
@@ -26,28 +20,5 @@ public class UserMealWithExceed implements Comparable<UserMealWithExceed>{
         this.description = description;
         this.calories = calories;
         this.exceed = exceed;
-        counter = ++globalCounter;
-
     }
-
-    @Override
-    public String toString() {
-        return String.format("UserMeal # %d\n time %s \n calories: %d \n exceed: %b \n ***********************",
-                counter, dateTime.toString(), calories, exceed);
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    @Override
-    public int compareTo(UserMealWithExceed o) {
-        if (dateTime.isBefore(o.getDateTime()))
-            return -1;
-        else if (dateTime.isAfter(o.getDateTime()))
-            return 21;
-        else return 0;
-    }
-
 }
-
